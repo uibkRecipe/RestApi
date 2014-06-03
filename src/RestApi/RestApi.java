fad0a5724bc02d7940f27796c933fe1be12298bc
suedtiroler <audi-s5@localhost.localdomain>
2014-06-03 11:14:14 +0200

    RestApi with doGet functions

--- src/RestApi/RestApi.java
+++ src/RestApi/RestApi.java
@@ -1,34 +1,45 @@
 package RestApi;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-
-
 public class RestApi {
-	
+
 	private static RestApi instance;
-	
-	private RestApi(){}
-	
-	public static RestApi getInstance(){
-		if(instance == null){
+
+	private RestApi() {
+	}
+
+	public static RestApi getInstance() {
+		if (instance == null) {
 			instance = new RestApi();
 		}
 		return instance;
 	}
-	public Boolean login(String username, String password){
+
+	/****************************************************************
+	 * 
+	 *  User Functionalities
+	 *  
+	 ****************************************************************/
+
+	public Boolean login(String username, String password) {
 		String test = doGet("http://138.232.65.234:8080/RestServer/rest/user/login/"
 				+ username + "/" + password);
 		ObjectMapper mapper = new ObjectMapper();
@@ -41,6 +52,339 @@ public class RestApi {
 		}
 		return o;
 	}
+
+//	public Boolean addUser(String username, String password, String email,
+//			String firstname, String lastname) {
+//		User newUser = new User(username, password, email, firstname, lastname);
+//		ObjectMapper mapper = new ObjectMapper();
+//		Writer strWriter = new StringWriter();
+//
+//		try {
+//			mapper.writeValue(strWriter, newUser);
+//		} catch (JsonGenerationException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		} catch (JsonMappingException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		String userDataJSON = strWriter.toString();
+//
+//		try {
+//			HttpResponse response = doPost(
+//					"http://138.232.65.234:8080/RestServer/rest/user/register",
+//					userDataJSON);
+//		} catch (ClientProtocolException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		return false;
+//	}
+	
+	/****************************************************************
+	 * 
+	 * Country Functionalities
+	 * 
+	 ****************************************************************/
+
+	public boolean getCountryList() {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/country");
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public boolean getCountryByCode(String countryCode) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/countrybycode/"
+				+ countryCode);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public boolean findCountryByName(String countryName) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/country/"
+				+ countryName);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public boolean findCountryCodeByName(String countryName) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/countrycode/"
+				+ countryName);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	/*****************************************************************
+	 * 
+	 * City Functionalities
+	 * 
+	 *****************************************************************/
+	
+	public boolean findCityById(int cityId) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/city/"
+				+ cityId);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	public boolean findCityByName(String cityName) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/city/"
+				+ cityName);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public boolean findCityByCountryAndRegion(String country, String region) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/city/"
+				+ country + "/" + region);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	/******************************************************************
+	 * 
+	 * Friends functionalities
+	 * 
+	 ******************************************************************/ 
+	
+	public boolean getFriend(String username) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/friend/"
+				+ username);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+//	public boolean addFriend() {
+//		
+//	}
+//	
+//	public boolean deleteFriend() {
+//		
+//	}
+	
+	public boolean existFriend(String username1, String username2) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/friend/"
+				+ username1 + "/" + username2);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	/****************************************************************
+	 *  
+	 * Recipe functionalities
+	 *
+	 ****************************************************************/
+	
+//	public boolean addRecipe() {
+//		
+//	}
+	
+	public boolean findRecipeByAutor(String author) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/recipe/"
+				+ author);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+//	public boolean removeRecipe() {
+//		
+//	}
+	
+	/***************************************************************
+	 * 
+	 * IngredientType manager
+	 * 
+	 ***************************************************************/
+	public boolean getAllIngredientType() {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/ingredientType/");
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	public boolean findIngredientByName(String ingredientname) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/ingredientType/"
+				+ ingredientname);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+//	public boolean addIngredientType() {
+//		
+//	}
+	
+	/***********************************************************
+	 * 
+	 * Composed Of
+	 * 
+	 ***********************************************************/
+	
+	public boolean getIngredients(int recipeId) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/ingredient/"
+				+ recipeId);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	public boolean findRezeptByIngredient() {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/recipe/ingredients");
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+//	public boolean addIngredientToRecipe() {
+//		
+//	}
+	
+	/****************************************************************
+	 * 
+	 * Ingredient
+	 * 
+	 ****************************************************************/
+	
+//	public boolean addIngredient() {
+//		
+//	}
+		
+	/*****************************************************************
+	 * 
+	 * Rating 
+	 * 
+	 *****************************************************************/
+	
+//	public boolean addRating() {
+//		
+//	}
+	
+	/****************************************************************
+	 * 
+	 * Region
+	 * 
+	 *****************************************************************/
+	
+	public boolean getRegionByCountryCode(String countryCode) {
+		String test = doGet("http://138.232.65.234:8080/RestServer/rest/region/" + countryCode);
+		ObjectMapper mapper = new ObjectMapper();
+		Boolean o = null;
+		try {
+			o = mapper.readValue(test, Boolean.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
+	}
+	
+	
+	/****************************************************************
+	 * 
+	 * Functions: 	doGet()		doPost()	convertStreamToString()
+	 *  
+	 ****************************************************************/
 	
 	public String convertStreamToString(InputStream is) throws IOException {
 		if (is != null) {
@@ -60,8 +404,7 @@ public class RestApi {
 			return "";
 		}
 	}
-	
-	
+
 	public String doGet(String url) {
 		String result = null;
 		HttpClient httpclient = new DefaultHttpClient();
@@ -93,4 +436,21 @@ public class RestApi {
 		// Return the json
 		return result;
 	}
+
+	public static HttpResponse doPost(String url, String string)
+			throws ClientProtocolException, IOException {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost request = new HttpPost(url);
+		StringEntity s = new StringEntity(string);
+
+		s.setContentEncoding("UTF-8");
+		s.setContentType("application/json");
+
+		request.setEntity(s);
+		request.addHeader("accept", "application/json");
+		request.addHeader("Content-type", "application/json");
+
+		return httpclient.execute(request);
+	}
+
 }
